3e2178681a019d157a61480583eb9108109ca042
Lauren <lat.mail.lat@gmail.com>
2024-06-12 14:25:56 +0100

    fixed the test file extension to tsx to match the componet file extension and fixed the eslint complaints in the .json

---
+++ src/pages/components/WeatherCard.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import WeatherCard from './WeatherCard';
+
+describe('Baisic weather card test', () => {
+  it('renders the date', () => {
+    render(<WeatherCard />);
+    expect(screen.getByTestId('date')).toBeInTheDocument();
+  });
+});
